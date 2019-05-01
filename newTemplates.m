c6dba1869a7f1197536599fedcfd4bd49e1ef87b
Arnau Marti <arnau.martif@estudiants.urv.cat>
2019-05-01 12:40:47 +0200

    added chars from other project for testing + final count

--- newTemplates.m
+++ newTemplates.m
@@ -1,22 +1,22 @@
 %CREATE TEMPLATES
 %Letter
-A=imread('Letters/A.bmp');B=imread('Letters/B.bmp');
-C=imread('Letters/C.bmp');D=imread('Letters/D.bmp');
-E=imread('Letters/E.bmp');F=imread('Letters/F.bmp');
-G=imread('Letters/G.bmp');H=imread('Letters/H.bmp');
-I=imread('Letters/I.bmp');J=imread('Letters/J.bmp');
-K=imread('Letters/K.bmp');L=imread('Letters/L.bmp');
-M=imread('Letters/M.bmp');N=imread('Letters/N.bmp');
-O=imread('Letters/O.bmp');P=imread('Letters/P.bmp');
-Q=imread('Letters/Q.bmp');R=imread('Letters/R.bmp');
-S=imread('Letters/S.bmp');T=imread('Letters/T.bmp');
-U=imread('Letters/U.bmp');V=imread('Letters/V.bmp');
-W=imread('Letters/W.bmp');X=imread('Letters/X.bmp');
-Y=imread('Letters/Y.bmp');Z=imread('Letters/Z.bmp');
+A=imread('char/A.bmp');B=imread('char/B.bmp');
+C=imread('char/C.bmp');D=imread('char/D.bmp');
+E=imread('char/E.bmp');F=imread('char/F.bmp');
+G=imread('char/G.bmp');H=imread('char/H.bmp');
+I=imread('char/I.bmp');J=imread('char/J.bmp');
+K=imread('char/K.bmp');L=imread('char/L.bmp');
+M=imread('char/M.bmp');N=imread('char/N.bmp');
+O=imread('char/O.bmp');P=imread('char/P.bmp');
+Q=imread('char/Q.bmp');R=imread('char/R.bmp');
+S=imread('char/S.bmp');T=imread('char/T.bmp');
+U=imread('char/U.bmp');V=imread('char/V.bmp');
+W=imread('char/W.bmp');X=imread('char/X.bmp');
+Y=imread('char/Y.bmp');Z=imread('char/Z.bmp');
 
 
 %*-*-*-*-*-*-*-*-*-*-*-
-letter=[A B C D  E F G H I J K L M N O P Q R S T U V W X Y Z];
+letter=[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z];
 
 character=[letter];
 
